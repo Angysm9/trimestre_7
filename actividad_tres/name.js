33b7cbbc3042c239f03e7fb8330d2540033e51a1
Angysm9 <2233073055@correo.ler.uam.mx>
2025-11-28 23:30:09 -0600

    casa

---
+++ actividad_tres/name.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(1340, 590)
+  background(200);
+}
+
+function draw() {
+  background(250, 170, 255);
+  noStroke()
+  fill(75, 43, 99)
+  rect(500,190,450,450)
+  fill(74, 118, 148)
+  triangle( 725, 30, 500, 190, 950, 190)
+  fill(170, 143, 201)
+  rect(565, 440, 120, 200)
+  fill(184, 18, 21)
+  ellipse(120, 120, 200);
+}
